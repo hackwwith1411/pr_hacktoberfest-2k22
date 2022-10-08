2ba75840c27ff74a25209b302cabafa8bd9f3a52
Nitesh Kumar <33413866+niteshk700@users.noreply.github.com>
2022-10-08 19:47:03 +0530

    Create Police and Thief.cpp
    codechef

---
+++ Police and Thief.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int t,x,y;
+	cin>>t;
+	while(t--){
+		cin>>x>>y;
+		if(x>=y){
+			cout<<x-y<<endl;
+		}else{
+				cout<<y-x<<endl;
+		}
+	}
+}
